11a463428d6f4f604887664d48991123ebffeaa9
marceloungaretti <marcelo.ungaretti@gmail.com>
2023-01-09 13:03:16 -0300

    adding routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,19 +2,27 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { Container } from 'react-bootstrap'
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import SignupScreen from './screens/SignupScreen';
+import LoginScreen from './screens/LoginScreen';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main>
         <Container>
-          <h1>React app is working!</h1>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='/signup' element={<SignupScreen />} />
+            <Route path='/login' element={<LoginScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
