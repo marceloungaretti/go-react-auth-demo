56cd9f541b11d745ed5186072fb7a37ab4c72ca7
marceloungaretti <marcelo.ungaretti@gmail.com>
2023-01-09 12:49:09 -0300

    frontend created with footer and header

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Container>
+          <h1>React app is working!</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
