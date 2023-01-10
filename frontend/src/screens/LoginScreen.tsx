67ad6c9d5e0060b943d910aa5dc675555792f29b
marceloungaretti <marcelo.ungaretti@gmail.com>
2023-01-10 00:49:10 -0300

    login screen implemented

--- frontend/src/screens/LoginScreen.tsx
+++ frontend/src/screens/LoginScreen.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Button, Form } from 'react-bootstrap'
+import FormContainer from '../components/FormContainer'
 
 const LoginScreen = () => {
   return (
-    <div>LoginScreen</div>
+    <FormContainer>
+      <h1>Login</h1>
+      <Form>
+        <Form.Group className="my-3" controlId="email">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter your email" />
+
+        </Form.Group>
+
+        <Form.Group className="my-3" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit" className='my-3'>
+          Submit
+        </Button>
+      </Form>
+    </FormContainer>
   )
 }
 
